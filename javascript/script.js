41967bf6deb692fe21b9cfbde8b8cd86ed922f12
jessedegroothva <jesse.de.groot2@hva.nl>
2023-03-15 15:31:53 +0100

    routie

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,23 @@
 import { fetchData } from "./fetch.js";
+import { overMij } from "./populate.js";
 
 fetchData()
+handleRoute()
+
+function handleRoute(){
+    routie({
+        // startpagina
+        '': () => {
+            fetchData();
+        },
+        // haal data op van id uit fetchData
+        // waarom heb je de id nodig? voor de laadtijd te verminderen?
+        // een nieuwe pagina maken?
+
+        // hier wordt de id uit de url gehaald
+        'overmij': () => {
+            // hier wordt de functie artData aangeroepen met de id uit de url die opgehaald wordt uit api.js
+            overMij();
+        }
+    })
+};
